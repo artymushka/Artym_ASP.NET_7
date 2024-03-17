69879a8a9618705f4d651242a813d574fc40abb6
Vlad_Artym <vladartym1@gmail.com>
2024-03-17 19:37:42 +0200

    add commit

---
+++ WebApplication1/Controllers/DataController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace WebApplication1.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateData(string customName, string customContent, string customFileName)
+        {
+            string dataContent = $"Custom Name: {customName}\nCustom Content: {customContent}";
+            string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "DataFiles");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string filePath = Path.Combine(directoryPath, customFileName + ".txt");
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(dataContent);
+            }
+
+            return PhysicalFile(filePath, "text/plain", customFileName + ".txt");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
